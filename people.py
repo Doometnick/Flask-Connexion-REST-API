c1920a038cc99a77a974d296d4cdb856e6b8cf4b
Dom <brandl.dominique@gmail.com>
2020-12-13 16:23:53 +0100

    Updated code for SQLAlchemy db

--- people.py
+++ people.py
@@ -3,35 +3,9 @@ This is the people module and supports all the ReST actions for the
 PEOPLE collection
 """
 
-# System modules
-from datetime import datetime
-
-# 3rd party modules
 from flask import make_response, abort
-
-
-def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
-
-
-# Data to serve with our API
-PEOPLE = {
-    "Farrell": {
-        "fname": "Doug",
-        "lname": "Farrell",
-        "timestamp": get_timestamp(),
-    },
-    "Brockman": {
-        "fname": "Kent",
-        "lname": "Brockman",
-        "timestamp": get_timestamp(),
-    },
-    "Easter": {
-        "fname": "Bunny",
-        "lname": "Easter",
-        "timestamp": get_timestamp(),
-    },
-}
+from config import db
+from models import Person, PersonSchema
 
 
 def read_all():
@@ -40,29 +14,25 @@ def read_all():
     with the complete lists of people
     :return:        json string of list of people
     """
-    # Create the list of people from our data
-    return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
+    people = Person.query.order_by(Person.lname).all()
+    person_schema = PersonSchema(many=True)
+    data = person_schema.dump(people)
+    return data
 
-
-def read_one(lname):
+def read_one(person_id):
     """
     This function responds to a request for /api/people/{lname}
     with one matching person from people
     :param lname:   last name of person to find
     :return:        person matching last name
     """
-    # Does the person exist in people?
-    if lname in PEOPLE:
-        person = PEOPLE.get(lname)
+    person = Person.query.filter(Person.person_id==person_id).one_or_none()
 
-    # otherwise, nope, not found
+    if person is not None:
+        person_schema = PersonSchema()
+        return person_schema.dump(person)
     else:
-        abort(
-            404, "Person with last name {lname} not found".format(lname=lname)
-        )
-
-    return person
-
+        abort(404, f"Person not found for Id: {person_id}.")
 
 def create(person):
     """
@@ -71,64 +41,86 @@ def create(person):
     :param person:  person to create in people structure
     :return:        201 on success, 406 on person exists
     """
-    lname = person.get("lname", None)
-    fname = person.get("fname", None)
+    lname = person.get("lname")
+    fname = person.get("fname")
 
-    # Does the person exist already?
-    if lname not in PEOPLE and lname is not None:
-        PEOPLE[lname] = {
-            "lname": lname,
-            "fname": fname,
-            "timestamp": get_timestamp(),
-        }
-        return make_response(
-            "{lname} successfully created".format(lname=lname), 201
-        )
+    existing_person = Person.query \
+        .filter(Person.fname == fname) \
+        .filter(Person.lname == lname) \
+        .one_or_none()
 
-    # Otherwise, they exist, that's an error
+    if existing_person is None:
+        schema = PersonSchema()
+        new_person = schema.load(person, session=db.session)
+
+        db.session.add(Person(fname=fname, lname=lname))
+        # db.session.add(new_person)
+        db.session.commit()
+
+        data = schema.dump(new_person)
+        return data, 201
+    
     else:
-        abort(
-            406,
-            "Person with last name {lname} already exists".format(lname=lname),
-        )
+        abort(409, f"Person {fname} {lname} already exists.")
 
-
-def update(lname, person):
+def update(person_id, person):
     """
     This function updates an existing person in the people structure
     :param lname:   last name of person to update in the people structure
     :param person:  person to update
     :return:        updated person structure
     """
-    # Does the person exist in people?
-    if lname in PEOPLE:
-        PEOPLE[lname]["fname"] = person.get("fname")
-        PEOPLE[lname]["timestamp"] = get_timestamp()
+    update_person = Person.query.filter(
+        Person.person_id == person_id
+    ).one_or_none()
 
-        return PEOPLE[lname]
+    fname = person.get("fname")
+    lname = person.get("lname")
 
-    # otherwise, nope, that's an error
-    else:
+    existing_person = (
+        Person.query.filter(Person.fname == fname)
+        .filter(Person.lname == lname)
+        .one_or_none()
+    )
+
+    if update_person is None:
         abort(
-            404, "Person with last name {lname} not found".format(lname=lname)
+            404,
+            f"Person not found for Id: {person_id}."
+        )
+    
+    elif existing_person is not None and existing_person.person_id != person_id:
+        abort(
+            409,
+            f"Person {fname} {lname} already exists."
         )
 
+    else:
+        update = Person(fname=fname, lname=lname)
+        update.person_id = person_id
+        schema = PersonSchema()
+        # update = schema.load(person, session=db.session)
+        # update.person_id = update_person.person_id
+        db.session.merge(update)
+        db.session.commit()
+        update_person = schema.load(person, session=db.session)
+        data = schema.dump(update_person)
+        return data, 200
 
-def delete(lname):
+def delete(person_id):
     """
     This function deletes a person from the people structure
     :param lname:   last name of person to delete
     :return:        200 on successful delete, 404 if not found
     """
-    # Does the person to delete exist?
-    if lname in PEOPLE:
-        del PEOPLE[lname]
+    person = Person.query.filter(Person.person_id == person_id).one_or_none()
+
+    if person is not None:
+        db.session.delete(person)
+        db.session.commit()
         return make_response(
-            "{lname} successfully deleted".format(lname=lname), 200
+            f"Person {person_id} ({person.fname} {person.lname}) deleted.", 200
         )
 
-    # Otherwise, nope, person to delete not found
     else:
-        abort(
-            404, "Person with last name {lname} not found".format(lname=lname)
-        )
+        abort(404, f"Person not found for Id: {person_id}.")
