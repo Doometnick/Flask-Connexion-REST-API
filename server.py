94e736a8aa322342cc5e2354442da0454211ccaf
Dom <brandl.dominique@gmail.com>
2020-12-13 16:24:17 +0100

    Refactoring, setup now done in config.py

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
-import connexion
-from flask import Flask, render_template
+import config
+from flask import render_template
 
 
 # Create the application instance
-app = connexion.App(__name__, specification_dir="./")
+connex_app = config.connex_app
 
 # Read swagger.yml file to configure the end points.
-app.add_api("swagger.yml")
+connex_app.add_api("swagger.yml")
 
 # Create a URL route in our application for "/"
-@app.route('/')
+@connex_app.route('/')
 def home():
     return render_template('home.html')
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    connex_app.run(debug=True)
